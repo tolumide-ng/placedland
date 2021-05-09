7c750acae841e41db525fc67223e16f3f749abd9
tolumide-ng <tolumideshopein@gmail.com>
2021-05-09 18:34:19 +0100

    creates job offer card

---
+++ src/main/components/UI/organisms/ExclusiveOffers/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import style from "./index.module.css";
+
+export const ExclusiveOffers = () => {
+    return (
+        <section>
+            <h2>Exclusive Job Offers</h2>
+        </section>
+    );
+};
---
+++ src/main/components/UI/organisms/JobOffer/index.tsx
@@ -0,0 +1,38 @@
+import { LoadImg } from "@atoms/LoadImg";
+import * as React from "react";
+import style from "./index.module.css";
+import employerOne from "@imgs/cm1.svg";
+import { Button } from "@atoms/Button";
+
+export const JobOffer = () => {
+    return (
+        <section className={style.job}>
+            <div className={style.jobContTop}></div>
+            <div className={style.jobCont}>
+                <div className={style.jobTop}>
+                    <div className={style.jobTopLeft}>
+                        <LoadImg
+                            loadHeight="72px"
+                            loadAlt="logo of the employer"
+                            loadImg={employerOne}
+                        />
+                    </div>
+                    <div className={style.jobTopRight}>
+                        <div className={style.jobTopRightTop}>
+                            <h4 className="">Gourment Burger Kitchen</h4>
+                            <p className="">Commis Waiter</p>
+                        </div>
+                        <div className={style.jobTopRightBottom}>
+                            <p>Fine dinning/Michelin Star</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={style.jobMid}></div>
+                <div className={style.jobBottom}>
+                    <Button buttonClass="" buttonText="Apply" />
+                    <Button buttonClass="" buttonText="shortList" />
+                </div>
+            </div>
+        </section>
+    );
+};
