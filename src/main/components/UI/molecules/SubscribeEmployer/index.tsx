df013266f76e52d0983ef6bb00d57f7f1d03f89f
tolumide-ng <tolumideshopein@gmail.com>
2021-05-09 18:09:47 +0100

    creates and styles employers follow info

--- src/main/components/UI/molecules/SubscribeEmployer/index.tsx
+++ src/main/components/UI/molecules/SubscribeEmployer/index.tsx
@@ -4,23 +4,28 @@ import { Button } from "@atoms/Button";
 
 export const SubscribeEmployer = () => {
     return (
-        <div className="">
-            <input
-                type="text"
-                name="email"
-                id="email"
-                placeholder="Your email address"
-                className={`${style.subscribeInput} ${style.subscribeEmail}`}
-            />
+        <div className={style.subscribe}>
+            <div className={style.subscribeCont}>
+                <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    placeholder="Your email address"
+                    className={`${style.subscribeInput} ${style.subscribeEmail}`}
+                />
 
-            <input
-                type="text"
-                name="name"
-                id="name"
-                placeholder="Your Name"
-                className={`${style.subscribeInput} ${style.subscribeName}`}
-            />
-            <Button buttonClass={style.subscribeButton} buttonText="follow" />
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Your Name"
+                    className={`${style.subscribeInput} ${style.subscribeName}`}
+                />
+                <Button
+                    buttonClass={style.subscribeButton}
+                    buttonText="follow"
+                />
+            </div>
         </div>
     );
 };
