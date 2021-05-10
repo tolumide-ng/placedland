ca604042ad029a74351b0e1384b4500b99697530
tolumide-ng <tolumideshopein@gmail.com>
2021-05-10 14:11:50 +0100

    creates th8e brandscard component

---
+++ src/main/components/UI/organisms/BrandsCard/index.tsx
@@ -0,0 +1,42 @@
+import { Button } from "@atoms/Button";
+import { LoadImg } from "@atoms/LoadImg";
+import * as React from "react";
+import style from "./index.module.css";
+
+interface BrandsCardDef {
+    brandImg: string;
+    brandLogo: string;
+    brandName: string;
+    brandMoto: string;
+}
+
+export const BrandsCard = (props: BrandsCardDef) => {
+    return (
+        <div className={style.brand}>
+            <div className={style.brandCard}>
+                <figure className={style.brandFig}>
+                    <LoadImg
+                        loadAlt={`image showing ${props.brandName}`}
+                        loadImg={props.brandImg}
+                        loadClass={style.brandImg}
+                    />
+                    <figcaption className={style.brandCaption}>
+                        <div className={style.brandCaptionCont}>
+                            <LoadImg
+                                loadAlt={`image showing the logo of ${props.brandName}`}
+                                loadImg={props.brandLogo}
+                                loadClass={style.brandLogo}
+                            />
+                            <Button
+                                buttonClass={style.brandButton}
+                                buttonText="Follow"
+                            />
+                        </div>
+                    </figcaption>
+                </figure>
+                <div className={style.brandName}>{props.brandName}</div>
+                <p className={style.brandMotto}>{props.brandMoto}</p>
+            </div>
+        </div>
+    );
+};
---
+++ src/main/components/UI/organisms/RelatedBrands/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import style from "./index.module.css";
+
+export const RelatedBrands = () => {
+    return (
+        <section>
+            <h3>Related Brands</h3>
+        </section>
+    );
+};
