1bce14ee0aca8c1d3e2d0cd271985afbad91338d
tolumide-ng <tolumideshopein@gmail.com>
2021-05-10 15:40:38 +0100

    creates the application components

---
+++ src/main/components/UI/organisms/UniqueApplication/index.tsx
@@ -0,0 +1,11 @@
+import { ApplyReason } from "@molecules/ApplyReason";
+import * as React from "react";
+import style from "./index.module.css";
+
+export const UniqueApplication = () => {
+    return (
+        <div className="">
+            <ApplyReason />
+        </div>
+    );
+};
