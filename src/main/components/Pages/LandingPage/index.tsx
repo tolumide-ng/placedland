a9b5e992a7f46549c041953bbfa3465150919cd2
tolumide-ng <tolumideshopein@gmail.com>
2021-05-10 01:17:05 +0100

    updates job offers styling

--- src/main/components/Pages/LandingPage/index.tsx
+++ src/main/components/Pages/LandingPage/index.tsx
@@ -1,4 +1,5 @@
 import { EmployerBasic } from "@organisms/EmployerBasic";
+import { ExclusiveOffers } from "@organisms/ExclusiveOffers";
 import * as React from "react";
 import "./index.css";
 
@@ -7,6 +8,8 @@ export const LandingPage = () => {
         <article className="ldpg">
             <article className="ldpg-cont">
                 <EmployerBasic />
+
+                {/* <ExclusiveOffers /> */}
             </article>
         </article>
     );
--- src/main/components/UI/organisms/JobOffer/index.tsx
+++ src/main/components/UI/organisms/JobOffer/index.tsx
@@ -7,7 +7,7 @@ import { Button } from "@atoms/Button";
 export const JobOffer = () => {
     return (
         <section className={style.job}>
-            <div className={style.jobContTop}></div>
+            {/* <div className={style.jobContTop}></div> */}
             <div className={style.jobCont}>
                 <div className={style.jobTop}>
                     <div className={style.jobTopLeft}>
@@ -15,11 +15,14 @@ export const JobOffer = () => {
                             loadHeight="72px"
                             loadAlt="logo of the employer"
                             loadImg={employerOne}
+                            loadClass={style.jobImage}
                         />
                     </div>
                     <div className={style.jobTopRight}>
                         <div className={style.jobTopRightTop}>
-                            <h4 className="">Gourment Burger Kitchen</h4>
+                            <h4 className={style.jobEmp}>
+                                Gourmet Burger Kitchen
+                            </h4>
                             <p className="">Commis Waiter</p>
                         </div>
                         <div className={style.jobTopRightBottom}>
